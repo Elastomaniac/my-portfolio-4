cba469b745a72aa522988162d91a3f944524cb39
Elastomaniac <vasyan89@gmail.com>
2022-07-28 17:53:45 +0300

    без усиленной домашки

--- scripts/script.js
+++ scripts/script.js
@@ -8,20 +8,26 @@ const purpose = +prompt('сколько Вы хотите накопить?')
 const extraMoney = +prompt(`Перечислите возможный доход за ваши дополнительные работы: ?`)
 
 function getAccumulatedIncome (money, amount, extraMoney) {
+  const result = money - amount + extraMoney
+  return result
 }
 
-const accumulatedIncome = getAccumulatedIncome(money - amount + extraMoney)
-const months = purpose / accumulatedIncome 
+const accumulatedIncome = getAccumulatedIncome(money, amount, extraMoney)
+const months = purpose / accumulatedIncome
 console.log(Math.ceil(months))
 
-function getTargetMonth () {
-
+function getTargetMonth (purpose, accumulatedIncome) {
+  return Math.ceil (purpose/accumulatedIncome)
 }
+
+const targetMonth = getTargetMonth(purpose, accumulatedIncome)
+
+
 
 const budgetDay = (money - amount + extraMoney) / 30 
 
-console.log('Ваш бюджет на месяц с учетом ваших расходов составляет: ', getAccumulatedIncome())
+console.log('Ваш бюджет на месяц с учетом ваших расходов составляет: ', accumulatedIncome)
 
-console.log(`Ваша цель накопить ${purpose} с учетом всех ваших расходов будет достигнута через`, getTargetMonth() + ' месяца')
+console.log(`Ваша цель накопить ${purpose} с учетом всех ваших расходов будет достигнута через`, targetMonth + ' месяца')
 
 console.log('Дневной бюджет', budgetDay)
