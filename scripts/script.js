56b971ed5baee13ae2357a9d2bf002d7f1c11b10
Elastomaniac <vasyan89@gmail.com>
2022-07-31 13:18:39 +0300

    все работает

--- scripts/script.js
+++ scripts/script.js
@@ -1,61 +1,32 @@
 'use strict'
 
-// let data = prompt('Угадай число от 1 до 10', 1)
-// let prNum = Math.floor((Math.random() * 10) + 1)
-
-// if (data == prNum) {
-//   alert('Вы угадали!')
-// } else if (data < prNum) {
-//   alert('Ваше число меньше загаданного компьютером')
-// } else if (data > prNum){
-//   alert('Ваше число больше загаданного компьютером')
-// } else {
-//   alert('Введи число!')
-// }
-
-//Замыкание
-
-// const funcOne = () => {
-//   let num = 4
-//   let user = +prompt('Угадайте загаданное число от 1 до 10')
-
-//   const funcTwo = () => {
-    
-//     if(num > user) {
-//       alert('Загаданное число меньше')
-//     } else if (num < user) {
-//       alert('Загаданное число больше')
-//     } else if (num == user) {
-//       alert('Поздравляю, Вы угадали!!!')
-//     } else {
-//       alert('Введи число!')
-//     }
-//     return
-//   }
-//   funcTwo()
-// }
-// funcOne()
-
-
-let prNum, tempOut
+let prNum, tempOut, attempts = 10
 
 prNum = Math.floor((Math.random() * 10) +1)
 // tempOut = document.getElementById('temp-out')
 // tempOut.innerHTML = prNum
 console.log(prNum)
 
-function f1() {
-  let num, out
+
+function startGame(num, out) {
+  attempts--
 
   num = document.getElementById('mynum').value
   out = document.getElementById('out')
 
   if (num == prNum) {
     out.innerHTML = 'Вы угадали!'
-  } else if (num > prNum) {
-    out.innerHTML = 'Вы ввели число больше, чем нужно!'
+  } 
+  if (attempts===0) {
+    out.innerHTML = 'У Вас закончились попытки, хотите сыграть еще?'
+    return
+  }
+
+  else if (num > prNum) {
+    console.log(attempts)
+    out.innerHTML = `Вы ввели число больше, чем нужно! У Вас осталось ${attempts} попыток`
   } else if (num < prNum) {
-    out.innerHTML = 'Вы ввели число меньше, чем нужно!'
+    out.innerHTML = `Вы ввели число меньще, чем нужно! У Вас осталось ${attempts} попыток`
   } else {
     out.innerHTML = 'Введите число от 1 до 10'
   }
