3e69ec5a41140c9b173c40f4e3666d0ca148de2e
Elastomaniac <vasyan89@gmail.com>
2022-08-07 14:10:06 +0300

    в процессе

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,29 @@
 'use strict'
 
-fetch('https://reqres.in/api/users?=per_page')
-  .then((res) => {
-    return res.json()
-  })
-  .then((body) => {
-    console.log(body)
-  })
+
+console.log('-----------');
+
+console.log('Пункт №1:')
+
+
+fetch('https://reqres.in/api/users?per_page=12')
+    .then((res) => {
+      return res.json()
+    })
+    .then((body) => {
+      console.log(body.data)
+      body.data.forEach((item) => {
+        if (item.first_name.length < 5) {
+        console.log(item.first_name)
+        }
+      })
+    })
+    .catch((e) => {
+      console.log(e)
+    })
+
+
+    
+console.log('-----------');
+
+console.log('Пункт №2:')
