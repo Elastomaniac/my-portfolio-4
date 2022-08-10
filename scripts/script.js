a613bcfe977982e127ba52b19761986933c94504
Elastomaniac <vasyan89@gmail.com>
2022-08-10 18:55:35 +0300

    первые 2 выполнены

--- scripts/script.js
+++ scripts/script.js
@@ -1,39 +1,13 @@
 'use strict'
-
-
-
-
-
 fetch('https://reqres.in/api/users?per_page=12')
     .then((res) => {
       return res.json()
     })
     .then((body) => {
       console.log(body.data)
-      body.data.forEach((item) => {
-        console.log(item.last_name)
-
-        body.data.filter (item => item.last_name.startsWith('F')).forEach(item=>{
-        console.log(item.last_name)
-        })
-
-        console.log(body.data.reduce((acc, item) => acc+ item.first_name+ ' ' + item.last_name + ',', )`Наша база содержит данные следующих пользователей:`)
-      })
-
-      Object.keys(body).forEach(item =>
-         console.log(item))
+      const filterResult = body.data.filter(item => item.last_name.startsWith('F'))
+      console.log(filterResult)
     })
     .catch((e) => {
       console.log(e)
     })
-
-    
-
-
-
-    
-console.log('-----------')
-
-console.log('Пункт №1:')
-
-console.log('-----------')
