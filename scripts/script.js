7e1b92fc894c1b2bddbc870d73277feb9892e86c
Elastomaniac <vasyan89@gmail.com>
2022-08-11 15:07:20 +0300

    первый разделитель работает корректно

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,21 @@
 'use strict'
 fetch('https://reqres.in/api/users?per_page=12')
     .then((res) => {
+      console.log('-----------')
+
+      console.log('Пункт №1:')
+
+      console.log('-----------')
       return res.json()
+      
     })
     .then((body) => {
       console.log(body.data)
         console.log(body.data.filter(item => item.last_name.startsWith('F')).reduce((acc, item) => acc + item.first_name + ' ' + item.last_name + ', ', ''))
 
         console.log('Наша база содержит данные следующих пользователей: ' + body.data.reduce((acc, item) => acc + item.first_name + ' ' + item.last_name + ', ', ''))
+
+        console.log(Object.keys(body.data))
     })
     .catch((e) => {
       console.log(e)
