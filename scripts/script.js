e6da419f17a377241778b72852979b9fe92bb973
Elastomaniac <vasyan89@gmail.com>
2022-08-11 15:29:28 +0300

    не вывел консоль фамилии всех пользователей в циулк

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,34 @@
 'use strict'
 fetch('https://reqres.in/api/users?per_page=12')
     .then((res) => {
-      console.log('-----------')
 
+      console.log('-----------')
       console.log('Пункт №1:')
-
       console.log('-----------')
+
       return res.json()
-      
     })
     .then((body) => {
       console.log(body.data)
-        console.log(body.data.filter(item => item.last_name.startsWith('F')).reduce((acc, item) => acc + item.first_name + ' ' + item.last_name + ', ', ''))
+      
 
-        console.log('Наша база содержит данные следующих пользователей: ' + body.data.reduce((acc, item) => acc + item.first_name + ' ' + item.last_name + ', ', ''))
+      console.log('-----------')
+      console.log('Пункт №3:')
+      console.log('-----------')
 
-        console.log(Object.keys(body.data))
+      console.log(body.data.filter(item => item.last_name.startsWith('F')).reduce((acc, item) => acc + item.first_name + ' ' + item.last_name + ', ', ''))
+
+      console.log('-----------')
+      console.log('Пункт №4:')
+      console.log('-----------')
+
+      console.log('Наша база содержит данные следующих пользователей: ' + body.data.reduce((acc, item) => acc + item.first_name + ' ' + item.last_name + ', ', ''))
+
+      console.log('-----------')
+      console.log('Пункт №5:')
+      console.log('-----------')
+
+      console.log(Object.keys(body.data))
     })
     .catch((e) => {
       console.log(e)
