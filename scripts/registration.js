1f4c981c6e6b2646f6dddfb985152a54bc3d9b29
Elastomaniac <vasyan89@gmail.com>
2022-08-18 16:43:33 +0300

    с Божьей помощью))

--- scripts/registration.js
+++ scripts/registration.js
@@ -2,13 +2,21 @@
 let emailRegistration = document.querySelector('#email-reg')
 let passwordRegistration = document.querySelector('#password-reg')
 let btnRegistration = document.querySelector('#btn-reg')
+
 let errorEmailRegistration = document.querySelector('#email-empty')
 let errorPasswordRegistration = document.querySelector('#password-empty')
+
 let passwordErrorLength = document.querySelector('#password-length')
 let emailValid = document.querySelector('#email-valid')
 
+let checkboxRegistration = document.querySelector('#check-reg')
+let checkboxError = document.querySelector('#check-empty')
+
 let emailResult = ''
 let passwordResult = ''
+let checkResult = false
+let user = {}
+let emailValidUser, passwordValid, checkedValid = false
 
 emailRegistration.addEventListener('input', (even) => {
   emailResult = even.target.value.trim()
@@ -18,17 +26,23 @@ passwordRegistration.addEventListener('input', (even) => {
   passwordResult = even.target.value.trim()
 })
 
+checkboxRegistration.addEventListener('change', (even) => {
+  checkResult = even.target.checked
+})
+
 btnRegistration.addEventListener('click', (even) => {
   even.preventDefault()
   validRegistration()
+  localDate()
 })
 
 const validRegistration = () => {
-  //проверка на пустые строки email и password
+  //проверка на пустую строку email
   if (emailResult.length == '') {
     errorEmailRegistration.style.display = 'block'
     emailRegistration.style.borderColor = 'red'
     emailValid.style.display = 'none'
+    emailValidUser = false
   } else {
     errorEmailRegistration.style.display = 'none'
     emailRegistration.style.borderColor = 'inherit'
@@ -38,13 +52,17 @@ const validRegistration = () => {
       emailValid.style.display = 'block'
     } else {
       emailValid.style.display = 'none'
+      user.email = emailResult
+      emailValidUser = true
     }
   }
 
+  //проверка на пустую строку password
   if (passwordResult.length == '') {
     errorPasswordRegistration.style.display = 'block'
     passwordRegistration.style.borderColor = 'red'
     passwordErrorLength.style.display = 'none'
+    passwordValid = false
   } else {
     errorPasswordRegistration.style.display = 'none'
     passwordRegistration.style.borderColor = 'inherit'
@@ -54,7 +72,17 @@ const validRegistration = () => {
       passwordErrorLength.style.display = 'block'
     } else {
       passwordErrorLength.style.display = 'none'
+      user.password = passwordResult
+      passwordValid = true
     }
+  }
+
+  if(!checkResult) {
+    checkboxError.style.display = 'block'
+    checkedValid = false
+  } else {
+    checkboxError.style.display = 'none'
+    checkedValid = true
   }
 }
 
@@ -64,4 +92,15 @@ const validateEmail = (email) => {
 
   return re.test(String(email).toLowerCase())
 }
+console.log(emailValidUser)
+console.log(passwordValid)
+console.log(checkedValid)
 
+
+const localDate = () => {
+  if (emailValidUser && passwordValid && checkedValid) {
+    console.log()
+    localStorage.user = JSON.stringify(user)
+    console.log(localStorage)
+  }
+}
