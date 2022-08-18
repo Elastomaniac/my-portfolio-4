b622d22e6125929e6f472dad64d8555ed635047d
Elastomaniac <vasyan89@gmail.com>
2022-08-18 13:57:35 +0300

    проверка валидности email выполнена

--- scripts/registration.js
+++ scripts/registration.js
@@ -4,6 +4,8 @@ let passwordRegistration = document.querySelector('#password-reg')
 let btnRegistration = document.querySelector('#btn-reg')
 let errorEmailRegistration = document.querySelector('#email-empty')
 let errorPasswordRegistration = document.querySelector('#password-empty')
+let passwordErrorLength = document.querySelector('#password-length')
+let emailValid = document.querySelector('#email-valid')
 
 let emailResult = ''
 let passwordResult = ''
@@ -22,6 +24,7 @@ btnRegistration.addEventListener('click', (even) => {
 })
 
 const validRegistration = () => {
+  //проверка на пустые строки email и password
   if (emailResult.length == '') {
     errorEmailRegistration.style.display = 'block'
     emailRegistration.style.borderColor = 'red'
@@ -37,5 +40,26 @@ const validRegistration = () => {
     errorPasswordRegistration.style.display = 'none'
     passwordRegistration.style.borderColor = 'inherit'
   }
+
+  //проверка на длину пароля
+  if (passwordResult.length < 8) {
+    passwordErrorLength.style.display = 'block'
+  } else {
+    passwordErrorLength.style.display = 'none'
+  }
+  //проверка валидности email поля
+  if(!validateEmail(emailResult)){
+    emailValid.style.display = 'block'
+  } else {
+    emailValid.style.display = 'none'
+  }
+}
+
+const validateEmail = (email) => {
+
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+  return re.test(String(email).toLowerCase())
+
 }
 
